469203a81b896acabb93422231ce18d5e7851582
valentinschabscheider <valentin.schabschneider@gmail.com>
2020-06-28 21:02:40 +0200

    made the secret key for the django app an environment variable

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ")hb==3kfh0fkla(ohmg@h=2ob94mrplx7w^tl)s4rugw_u=k^b"
+SECRET_KEY = get_env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
